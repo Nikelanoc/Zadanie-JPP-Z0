2eb5dc77cb7b4143fc982f17c700c7dbc33dbd57
Nikola <nikola.nocon@gmail.com>
2020-02-22 15:22:56 +0100

    Dodany plik

--- src/Zad1.java
+++ src/Zad1.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Zad1 {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("wpisz swoje imie");
+        String imie = scanner.nextLine();
+        System.out.println("Twoje imie to:" + imie);
 
+        System.out.println("wpisz swoje nazwisko");
+        String nazwisko = scanner.nextLine();
+        System.out.println("Twoje nazwisko :" + nazwisko);
+
+        System.out.println("wpisz swoj wiek");
+        int wiek = scanner.nextInt();
+        System.out.println("Twoj wiek to:" + wiek);
     }
 }
